cb8bb56a29ad20c3d9a9575451692113b3cdff31
Kaiel0904 <158366358+Kaiel0904@users.noreply.github.com>
2024-03-13 15:37:02 -0700

    Added rough draft for gieger lab

---
+++ pocketgieger_datagetter.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import datetime
+
+def my_callback(channel):
+    if GPIO.input(channel) == GPIO.LOW:
+        print('\nCount at ' + str(datetime.datetime.now()))
+ 
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
+    
+finally:
+    GPIO.cleanup()
