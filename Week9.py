1f5547463954b4e849630c65c76cde6dab11d021
Kaiel0904 <158366358+Kaiel0904@users.noreply.github.com>
2024-04-26 15:29:18 -0700

    making sure week9.py matches repository

--- Week9.py
+++ Week9.py
@@ -1,32 +1,49 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import csv
+import sys
 
 # Global variable to store count
+interval=0
+intervals = 30
 count = 0
+totalcounts = 0
+timestamps = []
+outfile = (sys.argv[1])
+meta_data = ['Time','Counts','Total Counts']
+file = open(outfile,"w")
+data_writer = csv.writer(file)
+data_writer.writerow(meta_data)
+
 
 def my_callback(channel):
     global count
+    global totalcounts
     count += 1
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now()))
+    totalcounts += 1
+    timestamp = datetime.datetime.now()
+    timestamps.append(timestamp)
+    print('Count detected at ' + str(timestamp))
+
 
 try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set up falling-edge event detection
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set up falling-edge event detection
 
     # Add falling edge detection with callback
-    GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback)
 
     # Infinite loop with sleep for 1 minute
-    while True:
+    while interval<intervals:
         # Sleep for 1 minute
-        time.sleep(60)
+        time.sleep(10)
         # Print the count and reset it
-        print(f"Counts collected in the last minute: {count}")
+        print(f"Counts collected in the last 10 seconds: {count}")
+        data_writer.writerow([time.time(),count,totalcounts])
         count = 0
+        interval +=1
+    file.close()
 
 except KeyboardInterrupt:
     print("\nKeyboard interrupt detected, exiting...")
